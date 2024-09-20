febc6f4f71b86a8d15820251f5a8aa5d64f07a0e
cheer <cheersupzoo@gmail.com>
2024-09-20 08:56:00 +0700

    fix: wrong env

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,5 @@
 export const onRequest = async (context) => {
-  const text = `Hello, world! ${process.env.test}`
+  const text = `Hello, world! ${context.env.test}`
 
   return new Response(text)
 }
