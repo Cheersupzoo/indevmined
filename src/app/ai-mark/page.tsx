2452156143ab757b129634b7b46c7c9fd781a05b
cheer <cheersupzoo@gmail.com>
2024-11-09 19:08:57 +0700

    feat: add mark the math mascot

--- src/app/ai-mark/page.tsx
+++ src/app/ai-mark/page.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout, { NormalResponsive } from '@/components/Layout'
 import MathQuestion from '@/components/MathQuestion'
-import { Pi } from 'lucide-react'
 import { Metadata } from 'next'
 import EnhancedMathBackground from './MathBackground'
 import './style.css'
@@ -8,19 +8,32 @@ import './style.css'
 export default function Home() {
   return (
     <Layout className='bg-slate-200 text-slate-800'>
-      <div className='fixed inset-0 bg-slate-200' />
+      <div className='-z-10 fixed inset-0 bg-slate-200' />
       <EnhancedMathBackground />
-      <NormalResponsive>
-        <div className='mt-8 text-4xl font-thin text-amber-700'>
-          <span className='bg-gradient p-1 rounded-xl text-slate-50 '><Pi size={36} className='inline mb-2' /> Mark</span> - The Mathematician AI
+      <NormalResponsive className='overflow-hidden sm:overflow-visible pb-4'>
+        <div className='relative flex '>
+          <div>
+            <div className='mt-4 text-xl sm:text-3xl font-thin text-amber-700'>
+              <span className='text-2xl sm:text-4xl bg-gradient p-1 rounded-xl text-slate-50 '>
+                Mark
+              </span>{' '}
+              - The Mathematician AI
+            </div>
+            <div className='mt-4 mb-4 text-sm text-slate-800'>
+              Mark is exceptional at calculations and counting. As a Generative
+              AI enhanced with a code interpreter, he can leverage programming
+              skills to overcome limitations that probability alone cannot
+              address. Why not put his abilities to the test?
+            </div>
+          </div>
+          <div className='w-[27rem] sm:w-[20rem] h-[12rem]' />
+          <img
+            className='absolute top-4 -right-10 sm:-right-16 w-[150px] sm:w-[250px]'
+            src='/mark.png'
+            alt='Mark the mathematician ai'
+          />
         </div>
-        <div className='mt-4 mb-4 text-sm text-slate-800'>
-          Mark is exceptional at calculations and counting. As a Generative AI
-          enhanced with a code interpreter, he can leverage programming skills
-          to overcome limitations that probability alone cannot address. Why not
-          put his abilities to the test?
-        </div>
-        <div className='bg-slate-50/50 shadow-lg p-2 rounded-3xl'>
+        <div className='bg-slate-50/70 shadow-lg p-2 rounded-3xl relative'>
           <MathQuestion />
           <div className='text-sm text-center text-zinc-500 mt-2'>
             Mark remains still far from perfection. Check important info.
