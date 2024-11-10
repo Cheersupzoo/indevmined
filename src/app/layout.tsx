a89f449624545d64428dd3240c8ea14a70b2a521
cheer <cheersupzoo@gmail.com>
2024-11-10 15:24:24 +0700

    feat: add simple analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,34 @@
-import type { Metadata } from "next";
-import { Ubuntu } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/utils/cn";
+/* eslint-disable @next/next/no-img-element */
+import type { Metadata } from 'next'
+import { Ubuntu } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/utils/cn'
+import Script from 'next/script'
 
-const inter = Ubuntu({ weight: ['300', '400', '500', '700'] ,subsets: ['greek'] });
+const inter = Ubuntu({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['greek']
+})
 
 export const metadata: Metadata = {
-  title: "404 | In Dev Mined",
-  description: "404 Page not found",
-};
+  title: '404 | In Dev Mined',
+  description: '404 Page not found'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body suppressHydrationWarning={true} className={cn(inter.className, 'bg-background')}>{children}</body>
+    <html lang='en'>
+      <body
+        suppressHydrationWarning={true}
+        className={cn(inter.className, 'bg-background')}
+      >
+        {children}
+      </body>
+      <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
     </html>
-  );
+  )
 }
