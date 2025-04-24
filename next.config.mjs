a8328e1b4b15393d4b561eb3de9e7ff0d6dd4c71
cheer <cheersupzoo@gmail.com>
2025-04-24 14:44:37 +0700

    fix: Yjs was already imported

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
+import path from 'path'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     // Ignore node-specific modules when bundling for the browser
     // See https://webpack.js.org/configuration/resolve/#resolvealias
     config.resolve.alias = {
       ...config.resolve.alias,
       'onnxruntime-node$': false
+    }
+
+    if (!isServer) {
+      // Ensure that all imports of 'yjs' resolve to the same instance
+      config.resolve.alias['yjs'] = path.resolve(import.meta.dirname, 'node_modules/yjs')
     }
 
     return config
