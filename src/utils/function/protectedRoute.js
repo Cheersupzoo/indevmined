8993cba81b2be18714931b29fbef662f65195870
cheer <cheersupzoo@gmail.com>
2025-04-29 11:24:55 +0700

    Revert "debug: Symbol.toStringTag"
    This reverts commit 06663d41d072932322c56f99d3e7202d01e04cb5.

--- src/utils/function/protectedRoute.js
+++ src/utils/function/protectedRoute.js
@@ -1,4 +1,4 @@
-import { jwtVerify, importJWK, decodeProtectedHeader } from 'jose'
+import { jwtVerify, decodeProtectedHeader } from 'jose'
 import { getHeader } from './index'
 
 const jwks_uri =
@@ -29,16 +29,10 @@ export const protectedRoute = (onRequest) => {
     }
     const token = authorization.replace(/^Bearer /, '')
     try {
-      const { kid, alg } = decodeProtectedHeader(token)
+      const { kid } = decodeProtectedHeader(token)
       const pubListReq = await fetch(new URL(jwks_uri))
       const pubList = await pubListReq.json()
       const jwk = pubList.keys.find((key) => key.kid === kid)
-      const JWK = await importJWK(jwk, alg)
-      console.log(
-        Symbol.toStringTag,
-        JWK[Symbol.toStringTag],
-        JWK[Symbol.toStringTag] === 'CryptoKey'
-      )
       const { payload } = await jwtVerify(token, jwk, {
         algorithms: ['RS256'],
         issuer: 'https://securetoken.google.com/indevmined',
