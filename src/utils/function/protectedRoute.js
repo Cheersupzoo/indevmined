8f5c868019194715babb70a8051ed1a2e31dc423
cheer <cheersupzoo@gmail.com>
2025-04-29 11:26:31 +0700

    Revert "fix: use jwk object instead"
    This reverts commit 477b8adc94f2e4d32e52a5d854bad09af2ca935d.

--- src/utils/function/protectedRoute.js
+++ src/utils/function/protectedRoute.js
@@ -1,4 +1,9 @@
-import { jwtVerify, decodeProtectedHeader } from 'jose'
+import {
+  jwtVerify,
+  createRemoteJWKSet,
+  importJWK,
+  decodeProtectedHeader
+} from 'jose'
 import { getHeader } from './index'
 
 const jwks_uri =
@@ -28,12 +33,16 @@ export const protectedRoute = (onRequest) => {
       )
     }
     const token = authorization.replace(/^Bearer /, '')
+    const JWKS = createRemoteJWKSet(new URL(jwks_uri))
     try {
-      const { kid } = decodeProtectedHeader(token)
+      const { kid, alg } = decodeProtectedHeader(token)
+      console.log('kid, alg', kid, alg)
+
       const pubListReq = await fetch(new URL(jwks_uri))
       const pubList = await pubListReq.json()
       const jwk = pubList.keys.find((key) => key.kid === kid)
-      const { payload } = await jwtVerify(token, jwk, {
+      const JWK = await importJWK(jwk, alg)
+      const { payload } = await jwtVerify(token, JWK, {
         algorithms: ['RS256'],
         issuer: 'https://securetoken.google.com/indevmined',
         audience: 'indevmined'
@@ -48,6 +57,7 @@ export const protectedRoute = (onRequest) => {
       return onRequest(context, payload)
     } catch (e) {
       console.error(e)
+      console.error('jwks', JWKS.jwks())
 
       return Response.json(
         { status: 'unauthorized', message: 'Invalid token' },
