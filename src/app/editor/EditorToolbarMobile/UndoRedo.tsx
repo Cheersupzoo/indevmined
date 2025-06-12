4905b2574a588fcd942d214b20e3558bc2edee21
cheer <cheersupzoo@gmail.com>
2025-06-12 14:24:40 +0700

    fix: missing UndoRedo rerender

--- src/app/editor/EditorToolbarMobile/UndoRedo.tsx
+++ src/app/editor/EditorToolbarMobile/UndoRedo.tsx
@@ -1,35 +1,58 @@
 import { cn } from '@/lib/utils'
-import { Editor } from '@tiptap/react'
+import { Memo, useObservable } from '@legendapp/state/react'
+import { Editor, EditorEvents } from '@tiptap/react'
 import { Redo2Icon, Undo2Icon } from 'lucide-react'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const UndoRedo = ({ editor }: { editor: Editor }) => {
+  const canUndo = useObservable(false)
+  const canRedo = useObservable(false)
+
+  useEffect(() => {
+    const onUndoChange = ({ editor }: EditorEvents['transaction']) => {
+      canUndo.set(editor.can().chain().focus().undo().run())
+    }
+    const onRedoChange = ({ editor }: EditorEvents['transaction']) => {
+      canRedo.set(editor.can().chain().focus().redo().run())
+    }
+    editor.on('transaction', onUndoChange)
+    editor.on('transaction', onRedoChange)
+    return () => {
+      editor.off('transaction', onUndoChange)
+      editor.off('transaction', onRedoChange)
+    }
+  }, [editor])
+
   return (
     <>
-      <button
-        className={cn(
-          !editor?.can().chain().focus().undo().run() && 'text-eva-text-border'
+      <Memo>
+        {() => (
+          <button
+            className={cn(!canUndo.get() && 'text-eva-text-border')}
+            onClick={(e) => {
+              e.preventDefault()
+              editor?.chain().focus().undo().run()
+            }}
+            disabled={!canUndo.get()}
+          >
+            <Undo2Icon size={16} />
+          </button>
         )}
-        onClick={(e) => {
-          e.preventDefault()
-          editor?.chain().focus().undo().run()
-        }}
-        disabled={!editor?.can().chain().focus().undo().run()}
-      >
-        <Undo2Icon size={16} />
-      </button>
-      <button
-        className={cn(
-          !editor?.can().chain().focus().redo().run() && 'text-eva-text-border'
+      </Memo>
+      <Memo>
+        {() => (
+          <button
+            className={cn(!canRedo.get() && 'text-eva-text-border')}
+            onClick={(e) => {
+              e.preventDefault()
+              editor?.chain().focus().redo().run()
+            }}
+            disabled={!canRedo.get()}
+          >
+            <Redo2Icon size={16} />
+          </button>
         )}
-        onClick={(e) => {
-          e.preventDefault()
-          editor?.chain().focus().redo().run()
-        }}
-        disabled={!editor?.can().chain().focus().redo().run()}
-      >
-        <Redo2Icon size={16} />
-      </button>
+      </Memo>
     </>
   )
 }
