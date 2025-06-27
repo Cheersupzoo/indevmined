18e34c7e8e67145be87ce5c909eec9247e76d3e6
cheer <cheersupzoo@gmail.com>
2025-06-27 12:55:40 +0700

    feat: remove unneed ssr: false

--- src/app/editor/extensions/nodes/DebugEditor/TypeRenderer.tsx
+++ src/app/editor/extensions/nodes/DebugEditor/TypeRenderer.tsx
@@ -4,18 +4,10 @@ import dynamic from 'next/dynamic'
 
 import './debugEditor.css'
 
-const Type1 = dynamic(() => import('./Type1').then((m) => m.Type1), {
-  ssr: false,
-})
-const Type2 = dynamic(() => import('./Type2').then((m) => m.Type2), {
-  ssr: false,
-})
-const Type3 = dynamic(() => import('./Type3').then((m) => m.Type3), {
-  ssr: false,
-})
-const Type4 = dynamic(() => import('./Type4').then((m) => m.Type4), {
-  ssr: false,
-})
+const Type1 = dynamic(() => import('./Type1').then((m) => m.Type1))
+const Type2 = dynamic(() => import('./Type2').then((m) => m.Type2))
+const Type3 = dynamic(() => import('./Type3').then((m) => m.Type3))
+const Type4 = dynamic(() => import('./Type4').then((m) => m.Type4))
 
 export const TypeRenderer = ({ type }: { type: number }) => {
   return (
