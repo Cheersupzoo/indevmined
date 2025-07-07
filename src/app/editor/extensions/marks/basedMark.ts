260b381f79e02dc40c452cfeaae6bc74770c0eca
cheer <cheersupzoo@gmail.com>
2025-07-07 19:18:33 +0700

    feat: try private package registry

--- src/app/editor/extensions/marks/basedMark.ts
+++ src/app/editor/extensions/marks/basedMark.ts
@@ -1,17 +1,3 @@
-import { type Extensions } from '@tiptap/core'
-import Underline from '@tiptap/extension-underline'
+import { basedMark } from '@cheersupzoo/editor/extensions/marks/based'
 
-import { AnimationMark } from './Animation'
-import { IdMark } from './Id/Id'
-import { CodeMark } from './MarkExtension'
-import { Playful } from './Playful/Playful'
-import { TextDecorationMark } from './TextDecorationMark'
-
-export const basedMarkExtensions: Extensions = [
-  Underline,
-  CodeMark,
-  Playful,
-  TextDecorationMark,
-  AnimationMark,
-  IdMark,
-]
+export const basedMarkExtensions = basedMark
